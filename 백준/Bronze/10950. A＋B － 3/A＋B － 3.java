900325e7d66f0bc27b1339d3a3d0acaa7dbacf6a
cloudmato <109732067+cloudmato@users.noreply.github.com>
2023-03-23 14:45:00 +0900

    [Bronze V] Title: A+B - 3, Time: 128 ms, Memory: 14348 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for (int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            sb.append(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+            sb.append('\n');
+        }
+        System.out.println(sb);
+
+    }}
