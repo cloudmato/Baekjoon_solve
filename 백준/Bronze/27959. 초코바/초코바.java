b64e57aebbac3c9e66937d565cf63ae098c5adab
cloudmato <109732067+cloudmato@users.noreply.github.com>
2024-10-19 20:43:12 +0900

    [Bronze V] Title: 초코바, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s =  br.readLine();
+
+        StringTokenizer st = new StringTokenizer(s," ");
+
+        int a =Integer.parseInt(st.nextToken()) * 100;
+        int b = Integer.parseInt(st.nextToken());
+
+        if( a >= b ){
+            System.out.println("Yes");
+        }
+        else System.out.println("No");
+
+    }
+}
